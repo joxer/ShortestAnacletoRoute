d6f1f22320a5df453fa520d9520b781eafc739b4
joxer <diego.luca.candido@gmail.com>
2016-03-15 20:18:09 +0100

    modified:   app/assets/stylesheets/style.css 	modified:   app/controllers/search_controller.rb 	modified:   app/views/search/home.html.erb 	public/img/logo.png

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,14 @@
 class SearchController < ApplicationController
   def search_route
-    render :json => Rome2Rio.get_from_a_to_b(params[:start_path],params[:end_path]).to_json
+    record = Route.where(start:  params[:start_path], end:  params[:end_path] ).first
+    
+    if record == nil
+      result = Rome2Rio.get_from_a_to_b(params[:start_path],params[:end_path]).to_json
+      Route.create(start: params[:start_path], end: params[:end_path], cache: result ,delta: DateTime.now)
+      render :json => result
+    else
+      render :json => record.cache
+    end
   end
 
   def home
